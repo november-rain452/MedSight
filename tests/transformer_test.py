a0cfa15513324ddf377d476e0676517790841601
Aditya Sinha <ranjanaditya452@gmail.com>
2026-06-06 04:39:16 +0530

    test now uses processor on top of everything

--- tests/transformer_test.py
+++ tests/transformer_test.py
@@ -1,10 +1,12 @@
 import pandas as pd
 from src.utils import transformer
+from src.ingest.processors import facility_to_documents
 
 df = pd.read_csv("databricks/Virtue Foundation Ghana v0.3 - Sheet1.csv")
 
-row = df.iloc[1].to_dict()
+row = df.iloc[0].to_dict()
 
 facility = transformer.transform_row(row)
+documents = facility_to_documents(facility)
 
-print(facility)
+print(documents)
