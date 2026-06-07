d887926f370614b522df2e9181515bfe512ead7f
Aditya Sinha <ranjanaditya452@gmail.com>
2026-06-08 04:15:23 +0530

    added a dot before imported module to let python know they belong i same directory, initpy for module onboard

--- src/database/vectors/vector_store.py
+++ src/database/vectors/vector_store.py
@@ -1,10 +1,10 @@
-from vector_store_config import collection
+from .vector_store_config import collection
 
 
 def add_documents(documents):
     for i, doc in enumerate(documents):
         collection.add(
-            docs=[doc["text"]],
+            documents=[doc["text"]],
             metadatas=[doc["metadata"]],
-            ids=[f"{doc['metadata']['facility_id']}_{doc['metadata'['type']]}_{i}"],
+            ids=[f"{doc['metadata']['facility_id']}_{doc['metadata']['type']}_{i}"],
         )
