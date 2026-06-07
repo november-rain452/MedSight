b674862718ddca6922a451ce627f11588f5c2a3b
Aditya Sinha <ranjanaditya452@gmail.com>
2026-06-08 04:15:55 +0530

    now every field is safely ingested into a string or it remains blank, no invalid values

--- src/utils/transformer.py
+++ src/utils/transformer.py
@@ -4,15 +4,15 @@ from .csv_field_parser import parse_csv_field_to_list, parse_csv_field_to_str
 def transform_row(row):
     return {
         "id": row.get("unique_id"),
-        "name": row.get("name"),
+        "name": parse_csv_field_to_str(row.get("name")),
         "specialties": parse_csv_field_to_list(row.get("specialties")),
         "procedure": parse_csv_field_to_list(row.get("procedure")),
         "equipment": parse_csv_field_to_list(row.get("equipment")),
         "capability": parse_csv_field_to_list(row.get("capability")),
         "description": parse_csv_field_to_str(row.get("description")),
         "location": {
-            "city": row.get("address_city"),
-            "country": row.get("address_country"),
+            "city": parse_csv_field_to_str(row.get("address_city")),
+            "country": parse_csv_field_to_str(row.get("address_country")),
         },
         "facility_type": row.get("facilityTypeId"),
     }
