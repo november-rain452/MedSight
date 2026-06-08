6d0fadc58fbaa1a4d4373acd8b06a81fe1b9704a
Aditya Sinha <ranjanaditya452@gmail.com>
2026-06-09 05:17:49 +0530

    testing output, queries and retrieval quality

--- tests/vectordb_test.py
+++ tests/vectordb_test.py
@@ -1,15 +1,17 @@
 import pandas as pd
-from src.database.vectors.vector_store import add_documents
-from src.utils.transformer import transform_row
+from src.retrieval.vector_store import add_documents, count_docs, query_documents
+from src.utils import transformer, formatter
 from src.ingest.processors import facility_to_documents
 from src.database.vectors.vector_store_config import collection
 
 df = pd.read_csv("databricks/Virtue Foundation Ghana v0.3 - Sheet1.csv")
-for i in range(2):
-    row = df.iloc[i].to_dict()
-    facility = transform_row(row)
-    document = facility_to_documents(facility)
-    add_documents(document)
+# for i in range(2):
+#     row = df.iloc[i].to_dict()
+#     facility = transformer.transform_row(row)
+#     document = facility_to_documents(facility)
+#     add_documents(document)
 
-result = collection.query(query_texts="chartered bank", n_results=2)
+query = "HIV outreach programs"
+output = query_documents(query)
+result = formatter.format_chroma_retrieval_output(output)
 print(result)
