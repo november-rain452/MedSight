a10ba2842eafecb923bd6c8793dd4c8b8ee6f347
Aditya Sinha <ranjanaditya452@gmail.com>
2026-06-10 23:57:18 +0530

    added a hash instead of loop iteration number for more deterministic unique id for our vector db

--- src/retrieval/vector_store.py
+++ src/retrieval/vector_store.py
@@ -1,12 +1,16 @@
 from ..database.vectors.vector_store_config import collection
 
+# add hashid instead of loop numbers
+import hashlib
+
 
 def add_documents(documents):
-    for i, doc in enumerate(documents):
+    for _, doc in enumerate(documents):
+        hash = hashlib.md5(doc["text"].encode("utf-8")).hexdigest()[:8]
         collection.add(
             documents=[doc["text"]],
             metadatas=[doc["metadata"]],
-            ids=[f"{doc['metadata']['facility_id']}_{doc['metadata']['type']}_{i}"],
+            ids=[f"{doc['metadata']['facility_id']}_{doc['metadata']['type']}_{hash}"],
         )
 
 
