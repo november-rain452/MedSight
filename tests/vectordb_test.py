301df2d2d5bf72955f7d2a3490047515efcde7c8
Aditya Sinha <ranjanaditya452@gmail.com>
2026-06-11 16:26:30 +0530

    testing with new distance threshold

--- tests/vectordb_test.py
+++ tests/vectordb_test.py
@@ -4,14 +4,14 @@ from src.utils import transformer, formatter
 from src.ingest.processors import facility_to_documents
 from src.database.vectors.vector_store_config import collection
 
-df = pd.read_csv("databricks/Virtue Foundation Ghana v0.3 - Sheet1.csv")
-# for i in range(2):
+# df = pd.read_csv("databricks/Virtue Foundation Ghana v0.3 - Sheet1.csv")
+# for i in range(10):
 #     row = df.iloc[i].to_dict()
 #     facility = transformer.transform_row(row)
 #     document = facility_to_documents(facility)
 #     add_documents(document)
 
-query = "HIV outreach programs"
-output = query_documents(query)
-result = formatter.format_chroma_retrieval_output(output)
-print(result)
+query = "HIV and AIDS"
+output = query_documents(query, n_results=5, doc_type="capability", dist_threshold=2)
+formatter.format_chroma_retrieval_output(output)
+print(count_docs())
