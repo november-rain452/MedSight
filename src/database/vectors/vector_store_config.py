5f1b1e36aa631e39b5d04652abc4f04e0c8e773f
Aditya Sinha <ranjanaditya452@gmail.com>
2026-06-11 16:25:11 +0530

    changed default l2euclidean distance to cosine similarity for vector db

--- src/database/vectors/vector_store_config.py
+++ src/database/vectors/vector_store_config.py
@@ -2,4 +2,6 @@ import chromadb
 
 chroma_client = chromadb.PersistentClient(path="./chroma_db")
 
-collection = chroma_client.get_or_create_collection(name="facilities")
+collection = chroma_client.get_or_create_collection(
+    name="facilities", metadata={"hnsw:space": "cosine"}
+)
