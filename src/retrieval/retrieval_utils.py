825b5ece545586911b12d957ef4063a785d87ab5
Aditya Sinha <ranjanaditya452@gmail.com>
2026-06-12 04:45:53 +0530

    helper functions for querying vector db

--- src/retrieval/retrieval_utils.py
+++ src/retrieval/retrieval_utils.py
@@ -11,3 +11,12 @@ def format_chroma_retrieval_output(output):
         print(f"Text : {docs[i]}")
         print(f"Metadata : {metas[i]}")
         print(f"Distance : {distances[i]}")
+
+
+def extract_keyword(query):
+    return [word.lower() for word in query.split() if len(word) >= 3]
+
+
+def if_keyword_exists(doc, query_keywords):
+    doc_lower = doc.lower()
+    return any(keyword in doc_lower for keyword in query_keywords)
