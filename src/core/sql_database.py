904fe8e14135e2f2b94ca6a146ed4373db1141ea
Aditya Sinha <ranjanaditya452@gmail.com>
2026-06-17 05:13:51 +0530

    msql config

---
+++ src/core/sql_database.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from .config import MYSQL_DATABASE_URL
+
+DB_URL = MYSQL_DATABASE_URL
+
+# engine
+engine = create_engine(DB_URL, echo=True)
+
+# factory
+SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    except Exception:
+        db.rollback()  # <-- Automatically roll back on errors
+        raise
+    finally:
+        db.close()
