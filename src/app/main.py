6da2477b4fdaba056cbec6035df64fe4b75e4cf7
Aditya Sinha <ranjanaditya452@gmail.com>
2026-06-18 14:04:52 +0530

    main py update to let orm handle table creation

--- src/app/main.py
+++ src/app/main.py
@@ -1,3 +1,6 @@
 from fastapi import FastAPI
+from ..core.sql_database import Base, engine
+from ..database.SQL.models.model import Facility, Freeform
 
 app = FastAPI()
+Base.metadata.create_all(bind=engine)
