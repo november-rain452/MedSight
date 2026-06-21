966a470851f2cdc10084dddb9731765fef66c45c
Aditya Sinha <ranjanaditya452@gmail.com>
2026-06-21 23:13:10 +0530

    return facility freeform by  repository

--- src/database/SQL/repository/internal_repository.py
+++ src/database/SQL/repository/internal_repository.py
@@ -1,7 +1,8 @@
-from ..schemas.facility_schema import FacilityCreate
-from ..schemas.freeform_schema import FreeformCreate
+from ..schemas.facility_schema import FacilityCreate, FacilityResponse
+from ..schemas.freeform_schema import FreeformCreate, FreeformResponse
 from ..models.model import Facility, Freeform
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, selectinload
+from sqlalchemy import select
 
 
 def insert_facility_freeform_repository(
@@ -18,3 +19,20 @@ def insert_facility_freeform_repository(
     except Exception:
         db.rollback()
         raise
+
+
+def get_facility_freeform_by_id(
+    db: Session, facility_id: int
+) -> FacilityResponse | None:
+    stmt = (
+        select(Facility)
+        .options(selectinload(Facility.freeform))
+        .where(Facility.id == facility_id)
+    )
+    result = db.execute(stmt)
+    facility = result.scalar_one_or_none()
+
+    if facility is None:
+        return None
+
+    return FacilityResponse.model_validate(facility)
