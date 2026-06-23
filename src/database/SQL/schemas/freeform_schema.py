ff9b08e42071c5dcad05faf58cebd5f2c443caa9
Aditya Sinha <ranjanaditya452@gmail.com>
2026-06-23 23:59:57 +0530

    pydantic schema now uses list of strings to store data instead of just strings

--- src/database/SQL/schemas/freeform_schema.py
+++ src/database/SQL/schemas/freeform_schema.py
@@ -2,9 +2,9 @@ from pydantic import BaseModel, ConfigDict
 
 
 class FreeformCreate(BaseModel):
-    procedure: str
-    equipment: str
-    capability: str
+    procedure: list[str]
+    equipment: list[str]
+    capability: list[str]
 
 
 class FreeformData(FreeformCreate):
@@ -15,6 +15,6 @@ class FreeformResponse(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     id: int
     facility_id: int
-    procedure: str
-    equipment: str
-    capability: str
+    procedure: list[str]
+    equipment: list[str]
+    capability: list[str]
