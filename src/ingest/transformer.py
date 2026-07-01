3f1c366ce3a06fa2ef5a234eece94b8b04fb9a09
Aditya Sinha <ranjanaditya452@gmail.com>
2026-07-02 05:27:17 +0530

    changed the way pandas derived rows are handled by transformer,, different datatype for every row

--- src/ingest/transformer.py
+++ src/ingest/transformer.py
@@ -3,19 +3,19 @@ from .csv_field_parser import parse_csv_field_to_list, parse_csv_field_to_none
 
 def transform_row(row):
     return {
-        "fid": parse_csv_field_to_none(row.get("unique_id")),
-        "name": parse_csv_field_to_none(row.get("name")),
-        "specialties": parse_csv_field_to_list(row.get("specialties")),
-        "procedure": parse_csv_field_to_list(row.get("procedure")),
-        "equipment": parse_csv_field_to_list(row.get("equipment")),
-        "capability": parse_csv_field_to_list(row.get("capability")),
-        "organization_type": parse_csv_field_to_none(row.get("organization_type")),
-        "description": parse_csv_field_to_none(row.get("description")),
+        "fid": parse_csv_field_to_none(row.unique_id),
+        "name": parse_csv_field_to_none(row.name),
+        "specialties": parse_csv_field_to_list(row.specialties),
+        "procedure": parse_csv_field_to_list(row.procedure),
+        "equipment": parse_csv_field_to_list(row.equipment),
+        "capability": parse_csv_field_to_list(row.capability),
+        "organization_type": parse_csv_field_to_none(row.organization_type),
+        "description": parse_csv_field_to_none(row.description),
         "location": {
-            "city": parse_csv_field_to_none(row.get("address_city")),
-            "country": parse_csv_field_to_none(row.get("address_country")),
+            "city": parse_csv_field_to_none(row.address_city),
+            "country": parse_csv_field_to_none(row.address_country),
         },
-        "facility_type": parse_csv_field_to_none(row.get("facilityTypeId")),
+        "facility_type": parse_csv_field_to_none(row.facilityTypeId),
     }
 
 
