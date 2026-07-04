77063bd8e4c50bb41badbf31e62ab18664e15a1b
Aditya Sinha <ranjanaditya452@gmail.com>
2026-07-05 03:20:32 +0530

    ingest route (for day 0 seeding)

---
+++ src/app/api/v1/endpoints/ingest_router.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, BackgroundTasks
+from src.ingest.ingest_orchestrator import ingest_orchestrator_func
+
+router = APIRouter(prefix="/ingest", tags=["Ingest"])
+
+
+@router.post("/seed-db", status_code=202)
+def seed_ingest_endpoint(background_tasks: BackgroundTasks):
+    """
+    Triggers the database seeding process in the background.
+    """
+    background_tasks.add_task(ingest_orchestrator_func)
+
+    return {
+        "status": "accepted",
+        "message": "Database seeding sequence initiated in the background.",
+    }
