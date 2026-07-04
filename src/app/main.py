a2e08a2db555df6ae98e294434e04d4e559a1f30
Aditya Sinha <ranjanaditya452@gmail.com>
2026-07-05 04:12:09 +0530

    added routers to main and fixed import issue

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
-from ..database.sql.config.sql_database import Base, engine
+from src.database.sql.config.sql_database import Base, engine
 from ..database.sql.models.model import Facility
+from .api.v1.router import api_router
 
 app = FastAPI()
+app.include_router(api_router)
 Base.metadata.create_all(bind=engine)
