52e7c82c34a84239717d2ee04aa51cad64c7ae7b
Aditya Sinha <ranjanaditya452@gmail.com>
2026-07-10 21:05:13 +0530

    made the query much more maintainable with a loop

--- src/retrieval/sql_search.py
+++ src/retrieval/sql_search.py
@@ -2,23 +2,21 @@ from ..database.sql.models.model import Facility
 from sqlalchemy import select
 from ..database.sql.services.api_services import execute_statement_service
 
+FIELD_MAP = {
+    "organization_type": Facility.organization_type,
+    "city": Facility.city,
+    "country": Facility.country,
+    "facility_type": Facility.facility_type,
+}
+
 
 def retrieve_sql(parsed_query: dict):
     stmt = select(Facility)
 
-    if parsed_query.get("organization_type"):
-        stmt = stmt.where(
-            Facility.organization_type == parsed_query["organization_type"]
-        )
-
-    if parsed_query.get("city"):
-        stmt = stmt.where(Facility.city == parsed_query["city"])
-
-    if parsed_query.get("country"):
-        stmt = stmt.where(Facility.country == parsed_query["country"])
-
-    if parsed_query.get("facility_type"):
-        stmt = stmt.where(Facility.facility_type == parsed_query["facility_type"])
+    for key, column in FIELD_MAP.items():
+        value = parsed_query.get(key)
+        if value:
+            stmt = stmt.where(column == value)
 
     results = execute_statement_service(stmt)
 
