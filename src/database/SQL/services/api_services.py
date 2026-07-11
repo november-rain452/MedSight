d96719699d04567c9c632c730f6670eb171fb8e6
Aditya Sinha <ranjanaditya452@gmail.com>
2026-07-11 09:31:38 +0530

    converted list[Facility] to list[FacilityResponse]

--- src/database/SQL/services/api_services.py
+++ src/database/SQL/services/api_services.py
@@ -1,8 +1,10 @@
 from ..config.sql_database import SessionLocal
 from ..repository.api_repository import execute_statement_repository
+from ..schemas.facility_schema import FacilityResponse
 
 
-def execute_statement_service(stmt):
+def execute_statement_service(stmt) -> list[FacilityResponse]:
     with SessionLocal() as db:
         facilities = execute_statement_repository(db, stmt)
-        return facilities
+        facility_responses = [FacilityResponse.model_validate(f) for f in facilities]
+        return facility_responses
