6b200beb828f843e5b58237a55d3dfdcb253a275
Aditya Sinha <ranjanaditya452@gmail.com>
2026-07-13 04:36:09 +0530

    added a field map and changed type datastructure from array to set

--- src/retrieval/vector_search.py
+++ src/retrieval/vector_search.py
@@ -1,11 +1,21 @@
-TYPE_ARRAY = ["procedure", "equipment", "capability"]
+TYPE_SET = {"procedure", "equipment", "capability"}
+
+FIELD_MAP = {
+    "organization_type": "level facility ",
+    "city": ", ",
+    "country": "",
+    "facility_type": "a type of ",
+    "procedure": "consisting of procedures like ",
+    "equipment": "has equipments such as ",
+    "capability": "with capabitilies such as ",
+}
 
 
 def retrieve_vectors(parsed_query: dict):
 
-    query_types = []
+    query_types = set()
 
-    for types in TYPE_ARRAY:
+    for types in TYPE_SET:
         if types in parsed_query:
             query_types.append(types)
 
