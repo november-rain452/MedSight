32321023abce2aab1d15d59759e8f757d4cf00ff
Aditya Sinha <ranjanaditya452@gmail.com>
2026-07-13 04:26:17 +0530

    added type aggregator

--- src/retrieval/vector_search.py
+++ src/retrieval/vector_search.py
@@ -1,5 +1,12 @@
-TYPE_MAP = {"procedure": {}, "equipment": {}, "capability": {}}
+TYPE_ARRAY = ["procedure", "equipment", "capability"]
 
 
 def retrieve_vectors(parsed_query: dict):
-    pass
+
+    query_types = []
+
+    for types in TYPE_ARRAY:
+        if types in parsed_query:
+            query_types.append(types)
+
+    query = f""
