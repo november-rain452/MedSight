e36a59c36916483e1d3a399ffa8c945227cfc3fa
Aditya Sinha <ranjanaditya452@gmail.com>
2026-07-15 16:33:23 +0530

    added return hints, converting orm objects to pydantic models

--- src/database/SQL/services/internal_services.py
+++ src/database/SQL/services/internal_services.py
@@ -1,4 +1,4 @@
-from ..schemas.facility_schema import FacilityCreate
+from ..schemas.facility_schema import FacilityCreate, FacilityResponse
 from ..config.sql_database import SessionLocal
 from ..repository.internal_repository import (
     insert_facility_repository,
@@ -10,7 +10,7 @@ from src.core.logger import logger
 
 
 # inserts
-def insert_facility_service(facility_data: FacilityCreate):
+def insert_facility_service(facility_data: FacilityCreate) -> FacilityResponse:
     logger.info(f"Attempting to insert facility: {facility_data.fid}")
     with SessionLocal() as db:
         try:
@@ -20,7 +20,7 @@ def insert_facility_service(facility_data: FacilityCreate):
             db.refresh(facility)
             logger.info(f"Facility inserted successfully with ID: {facility.id}")
 
-            return facility
+            return FacilityResponse.model_validate(facility)
         except DuplicateFacilityError as e:
             logger.warning(f"Duplicate facility detected: {facility_data.name}. {e}")
             raise
@@ -30,7 +30,7 @@ def insert_facility_service(facility_data: FacilityCreate):
             raise
 
 
-def insert_in_batch_service(batch_sql: list[dict]):
+def insert_in_batch_service(batch_sql: list[dict]) -> None:
 
     validated = [FacilityCreate.model_validate(row).model_dump() for row in batch_sql]
     logger.info(f"Starting batch insertion of {len(validated)} facilities")
@@ -48,16 +48,16 @@ def insert_in_batch_service(batch_sql: list[dict]):
 
 
 # gets
-def get_facility_by_id_service(facility_id: int):
+def get_facility_by_id_service(facility_id: int) -> FacilityResponse | None:
     logger.info(f"Retrieving facility by ID: {facility_id}")
     with SessionLocal() as db:
         try:
             facility = get_facility_by_id(db, facility_id)
             if facility is None:
                 logger.warning(f"Facility with facility id: {facility_id} not found")
-            else:
-                logger.info(f"Facility found: ID {facility.id}")
-            return facility
+                return None
+
+            return FacilityResponse.model_validate(facility)
         except Exception as e:
             logger.error(
                 f"Error retrieving facility ID {facility_id}: {e}", exc_info=True
