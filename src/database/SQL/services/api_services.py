8fe716bb1e3f05aa7a758c450b07b3d9577f1e3a
Aditya Sinha <ranjanaditya452@gmail.com>
2026-07-16 12:18:47 +0530

    added logging to statement

--- src/database/SQL/services/api_services.py
+++ src/database/SQL/services/api_services.py
@@ -1,10 +1,18 @@
 from ..config.sql_database import SessionLocal
 from ..repository.api_repository import execute_statement_repository
 from ..schemas.facility_schema import FacilityResponse
+from src.core.logger import logger
 
 
 def execute_statement_service(stmt) -> list[FacilityResponse]:
     with SessionLocal() as db:
-        facilities = execute_statement_repository(db, stmt)
-        facility_responses = [FacilityResponse.model_validate(f) for f in facilities]
-        return facility_responses
+        try:
+            logger.info(f"Executing sql statement: {stmt}")
+            facilities = execute_statement_repository(db, stmt)
+            facility_responses = [
+                FacilityResponse.model_validate(f) for f in facilities
+            ]
+            return facility_responses
+        except Exception as e:
+            logger.error(f"Error executing sql statement: {stmt}")
+            raise
